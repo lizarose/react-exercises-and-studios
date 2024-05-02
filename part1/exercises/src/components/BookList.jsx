1382fb2fbb690da0eb554e3e6c14653301b7f70b
Elizabeth Levinson <lizarose266@gmail.com>
2024-05-02 10:12:07 -0500

    Part 1 Completed

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Book Titles Released This Wee";
+   let book1 = "https://th.bing.com/th/id/OIP.LWELQFyyqAdZ6xy0pHkjHQAAAA?rs=1&pid=ImgDetMain";
+   let book2 = "https://th.bing.com/th/id/OIP.ASe4wcW_EJDOtThFCAMmRAAAAA?rs=1&pid=ImgDetMain";
+   let book3 = "https://th.bing.com/th/id/OIP.RJ-y1xw0LkEaIFhHT7sjPAHaLW?rs=1&pid=ImgDetMain";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="King of Sloth by Ana Huang" />
+         <img src={book2} alt="Powerful: A Powerless Story by Lauren Roberts" />
+         <img src={book3} alt="Only the Brave: A Novel by Danielle Steel" />
       </div>      
    );
 }
