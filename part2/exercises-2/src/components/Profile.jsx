a07e03a6f0da06549d5089f757a78b09b0480bb2
Elizabeth Levinson <lizarose266@gmail.com>
2024-05-06 09:39:48 -0500

    part 2 exercises

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,12 +1,30 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json'
+
+const listItem = oceans.map(oceans =>
+   <div key = {oceans.id} className= {`${oceans.fishCheck === "true" ? "isAFish" : "profile"}`}>
+      <img src='{oceans.image}' alt = {oceans.name} className='img' />
+   </div>
+   
+);
 
 
 function Profile()
 {
    return(
       <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
+      <h1>{oceans.name}</h1>
+         <h3>Fun Facts:</h3>
+         <ul>
+            <li>{oceans.fact1}</li>
+            <li>{oceans.fact2}</li>
+            <li>{oceans.fact3}</li>
+         </ul>
+
+        <ul>
+         {listItem}
+         </ul> 
          <Button />
       </>  
    );
